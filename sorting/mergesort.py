9c7ba1abf13290385b58dfa327a48e1d543fc138
Maes <11432396+maesbrisa@users.noreply.github.com>
2020-10-05 20:25:54 +0100

    using indexes and modifying the original list

--- sorting/mergesort.py
+++ sorting/mergesort.py
@@ -1,87 +1,42 @@
 
 
-def divide(temp_list, unsorted_list):
+def merge_sort(original_list):
     print('entering divide')
-    if len(unsorted_list) == 1:
-        temp_list.append(unsorted_list)
-    else:
-        divide(temp_list, unsorted_list[len(unsorted_list)//2:])
-        divide(temp_list, unsorted_list[:len(unsorted_list)//2])
-
-def merge_both(a=[], b=[]):
-    index, length, c = 0, len(a) + len(b), []
-    if len(a) == 0:
-        print('----- Final result -----')
-        print(b)
-        print('------------------------')
-        return b
-    if len(b) == 0:
-        print('----- Final result -----')
-        print(a)
-        print('------------------------')
-        return a    
-    while len(sorted_list) < length-1:
-        if list_to_sort[index] <= b[index]:
-            print(str(a[index]) + ' minor than ' + str(b[index]))
-            if len(c) > 1 and a[index] < c[-1]:
-                c.insert(-1, a[index])
+    if len(original_list) > 1:
+        middle_index = len(original_list)//2
+        left = original_list[:middle_index]
+        right = original_list[middle_index:]
+        merge_sort(left)
+        merge_sort(right)
+        print('i am back!')
+        left_index, right_index, original_index = 0, 0, 0
+        while left_index < len(left) and right_index < len(right):
+            if left[left_index] <= right[right_index]:
+                original_list[original_index] = left[left_index]
+                left_index += 1
             else:
-                c.append(a[index])
-            c.append(b[index])
-            print('Current C array')
-            print(c)
-        else:
-            print(str(a[index]) + ' major than ' + str(b[index]))
-            if len(c) > 1 and b[index] < c[-1]:
-                c.insert(-1, b[index])
-            else:
-                c.append(b[index])
-            c.append(a[index])
-            print('Current C array')
-            print(c)
-        if index == len(a) -1:
-            if index < len(b)-1:
-                print('there are elements left from: ' + str(index) )
-                print('elements left from ' + str(index))
-                print(b[index+1:])
-                c.extend(b[index+1:])
-                print('Check lengths')
-                print(length, len(c))
-        else:
-            if index == len(b)-1:
-                print('there are elements left from: ' + str(index) )
-                print('elements left from ' + str(index))
-                print(a[index+1:])
-                c.extend(a[index+1:])
-                print('Check lengths')
-                print(length, len(c))
-            else:
-                index += 1
-    print('----- Final result -----')
-    print(c)
-    print('------------------------')
-    return c
+                original_list[original_index] = right[right_index]
+                right_index += 1
+            original_index += 1
+            print('we have just put two values in order from both halves')
+            print(right[right_index-1], left[left_index-1])
 
-def merge(list_to_sort, length):
-    print('entering merge')
-    index, sorted_list = 0, []
-    print(list_to_sort)
-    for i in range(0, len(sorted_list), 2):
-        print('enter for loop')
-        if i+1 < len(sorted_list)-1:
-            print('merging both')
-            merge_both(sorted_list[i], sorted_list[i+1])
-        else:
-            print('we reached final elements')
-            merge_both(sorted_list[i], [])
-    return sorted_list
+        while left_index < len(left):
+            print('this means that right half is out of stock')
+            print(right, right_index, left, left_index)
+            original_list[original_index] = left[left_index]
+            left_index += 1
+            original_index += 1
 
-
-
+        while right_index < len(right):
+            print('this means that left half is out of stock')
+            print(right, right_index, left, left_index)
+            original_list[original_index] = right[right_index]
+            right_index += 1
+            original_index += 1
 
 if __name__ == '__main__':
-    temp_list = []
     list_to_sort = [5,4,2,7,9,1]
-    divide(temp_list, list_to_sort)
-    print(temp_list)
-    merge(temp_list, len(list_to_sort))
+    merge_sort(list_to_sort)
+    print('now sorted')
+    print(list_to_sort)
